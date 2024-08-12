0654e2a080517103af66b252c2c34b2fda619cd3
shyam-206 <shyamdhokiya15@gmail.com>
2024-08-12 17:15:34 +0530

    angular + .netcore project

---
+++ Angular + .Net Core/Server/CrudAPI/CrudAPI/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using CrudAPI.Models.Context;
+using CrudAPI.Models.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpPost("CreateCategory")]
+        public async Task<IActionResult> CreateCategory(CategoryModel categoryModel)
+        {
+            Category category = new Category();
+            category.Name = categoryModel.Name;
+
+            await db.Categories.AddAsync(category);
+            await db.SaveChangesAsync();
+
+            return Ok(new { message = "Category created successfully" });
+        }
+    }
+}
