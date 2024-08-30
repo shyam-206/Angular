986392493c54cdc0d0fc7e537f1b21d29c250291
shyam-206 <shyamdhokiya15@gmail.com>
2024-08-30 11:18:05 +0530

    done

--- Angular + .Net Core/Server/CrudAPI/CrudAPI/Models/ViewModel/CategoryModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CrudAPI.Models.ViewModel
-{
-    public class CategoryModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
